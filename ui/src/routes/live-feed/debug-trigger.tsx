9753e49eee139cee54e62a408a3926a75b19e476
Mark Redeman <mark.s.redeman@intel.com>
2025-07-14 10:23:00 +0200

    Add debug options in live feed

--- ui/src/routes/live-feed/debug-trigger.tsx
+++ ui/src/routes/live-feed/debug-trigger.tsx
@@ -8,6 +8,7 @@ import { useWebRTCConnection } from '../../components/stream/web-rtc-connection-
 
 const DebugTooltip = () => {
     const modelsQuery = $api.useSuspenseQuery('get', '/api/models');
+    const inputMutation = $api.useMutation('post', '/api/inputs');
     const activeModelMutation = $api.useMutation('post', '/api/models/{model_name}:activate');
 
     const { start, stop } = useWebRTCConnection();
@@ -44,6 +45,48 @@ const DebugTooltip = () => {
                     );
                 })}
             </Picker>
+
+            <Divider size='S' />
+
+            <Flex gap='size-200'>
+                <Button
+                    onPress={() => {
+                        inputMutation.mutateAsync({
+                            body: {
+                                source_type: 'webcam',
+                                device_id: 0,
+                            },
+                        });
+                    }}
+                >
+                    Cam 0
+                </Button>
+                <Button
+                    onPress={() => {
+                        inputMutation.mutateAsync({
+                            body: {
+                                source_type: 'video_file',
+                                video_path: 'data/media/video.mp4',
+                            },
+                        });
+                    }}
+                >
+                    Video 1
+                </Button>
+
+                <Button
+                    onPress={() => {
+                        inputMutation.mutateAsync({
+                            body: {
+                                source_type: 'video_file',
+                                video_path: 'data/media/video_long.mp4',
+                            },
+                        });
+                    }}
+                >
+                    Video 2
+                </Button>
+            </Flex>
         </Flex>
     );
 };
