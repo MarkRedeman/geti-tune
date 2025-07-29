d99c7c823b2a990149f3110c8f507e0621af258d
Mark Redeman <mark.s.redeman@intel.com>
2025-07-29 08:51:44 +0200

    Configure tauri with a sidecar

--- ui/src-tauri/src/lib.rs
+++ ui/src-tauri/src/lib.rs
@@ -1,16 +1,58 @@
+//use reqwest::Client;
+//use serde_json::Value;
+
+// #[tauri::command]
+// async fn py_api(method: String, endpoint: String, payload: Option<Value>) -> Result<Value, String> {
+//     let client = Client::new();
+//     let url = format!("http://127.0.0.1:8000/{}", endpoint);
+
+//     let request = match method.as_str() {
+//         "GET" => client.get(&url),
+//         "POST" => {
+//             let req = client.post(&url);
+//             if let Some(data) = &payload {
+//                 req.json(data)
+//             } else {
+//                 req
+//             }
+//         }
+//         "PUT" => client.put(&url),
+//         "DELETE" => client.delete(&url),
+//         _ => return Err(format!("Unsupported HTTP method: {}", method)),
+//     };
+
+//     let request = if let Some(data) = payload {
+//         request.json(&data)
+//     } else {
+//         request
+//     };
+
+//     let response = request
+//         .send()
+//         .await
+//         .map_err(|e| e.to_string())?
+//         .json::<Value>()
+//         .await
+//         .map_err(|e| e.to_string())?;
+
+//     Ok(response)
+// }
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+            Ok(())
+        })
+        //.invoke_handler(tauri::generate_handler![py_api])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
