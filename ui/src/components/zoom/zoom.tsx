83c74eb42418b8afd776529471d9d3498c7cd75a
Mark Redeman <mark.s.redeman@intel.com>
2025-07-31 13:17:57 +0200

    Allow setting the padding when auto zooming
    In the gallery we don't want to add padding

--- ui/src/components/zoom/zoom.tsx
+++ ui/src/components/zoom/zoom.tsx
@@ -1,9 +1,10 @@
 import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from 'react';
 
-export type ZoomState = { scale: number; translate: { x: number; y: number } };
+export type ZoomState = { scale: number; translate: { x: number; y: number }; initialScale: number };
 export const Zoom = createContext<ZoomState>({
     scale: 1.0,
     translate: { x: 0, y: 0 },
+    initialScale: 0.9,
 });
 const SetZoom = createContext<Dispatch<SetStateAction<ZoomState>> | null>(null);
 
@@ -21,13 +22,14 @@ export const useSetZoom = () => {
     return context;
 };
 
-export const ZoomProvider = ({ children }: { children: ReactNode }) => {
+export const ZoomProvider = ({ children, initialScale = 0.9 }: { children: ReactNode; initialScale?: number }) => {
     // TODO:
     // 1. Add scale restrictions - min max
     // 2. Add translate restrictions - min max
     const [zoom, setZoom] = useState<ZoomState>({
         scale: 1.0,
         translate: { x: 0, y: 0 },
+        initialScale,
     });
 
     return (
